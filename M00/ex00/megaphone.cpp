87e0d35ed81572e2ea03ff87fe32e246ce8bd993
heghine <heghine.balasanyan@gmail.com>
2023-10-10 14:04:37 +0400

    upd: ex00

---
+++ M00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbalasan <hbalasan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 14:00:14 by hbalasan          #+#    #+#             */
+/*   Updated: 2023/10/10 14:00:15 by hbalasan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i, j;
+
+	if (argc < 2)
+		std::cout << "*** LOUD NOISE ***";
+
+	i = 0;
+	while (argc > 1 && argv[++i])
+	{
+		j = -1;
+		while (argv[i][++j])
+			std::cout << (char)std::toupper(argv[i][j]);
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	return (0);
+}
