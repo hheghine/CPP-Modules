42a8bc34977fb109eb0206c320150a7022314173
heghine <heghine.balasanyan@gmail.com>
2023-10-13 03:56:06 +0400

    upd: ex01

--- M00/ex01/main.cpp
+++ M00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbalasan <hbalasan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:53:13 by hbalasan          #+#    #+#             */
-/*   Updated: 2023/10/11 02:44:05 by hbalasan         ###   ########.fr       */
+/*   Updated: 2023/10/13 03:46:04 by hbalasan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@ int	main(void)
 	Phonebook	pb;
 	std::string	str;
 
+	std::cout << "Command List" << std::endl;
+	std::cout << "\tADD:\tsave a new contact" << std::endl;
+	std::cout << "\tSEARCH:\tdisplay a specific contact" << std::endl;
+	std::cout << "\tEXIT" << std::endl;
 	while (str != "EXIT")
-	{
-		std::cout << "Command List" << std::endl;
-		std::cout << "\tADD:\tsave a new contact" << std::endl;
-		std::cout << "\tSEARCH:\tdisplay a specific contact" << std::endl;
-		std::cout << "\tEXIT" << std::endl;
-		
-		std::cout << "Enter a command > ";
+	{	
+		std::cout << "\nEnter a command > ";
 		std::getline(std::cin, str);
 		if (str == "ADD")
 			pb.add();
